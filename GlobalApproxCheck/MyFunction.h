a41c129f60a06397342beee8437c1070ff124c39
Alan Chen <alchen@silabs.com>
2017-07-20 13:49:18 -0500

    initialize instructions working

--- GlobalApproxCheck/MyFunction.h
+++ GlobalApproxCheck/MyFunction.h
@@ -15,20 +15,27 @@ class MyFunction {
 public:
   Function* root;
   std::vector<std::pair<MyInstruction, ApproxStatus>> args;
+  std::vector<MyInstruction> myInsts;
 
   MyFunction(Function* fun) {
     root = fun;
+    initializeInstructions();
     initializeArguments();
   }
 
 private:
   void initializeArguments() {
+    args.clear();
     const int NUM_ARGS = (root->arg_end() - root->arg_begin());
-    inst_iterator ii = inst_begin(root);
     for (int i = 0; i < NUM_ARGS; i++) {
-			MyInstruction mi(&*ii);
-			args.push_back(std::make_pair(mi, ApproxStatus::pending));
-      ii++;
+			args.push_back(std::make_pair(myInsts[i], ApproxStatus::pending));
+    }
+  }
+
+  void initializeInstructions() {
+    myInsts.clear();
+    for (inst_iterator ii = inst_begin(*root); ii != inst_end(*root); ii++) {
+      myInsts.push_back(MyInstruction(&*ii));;
     }
   }
 
